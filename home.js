80fde2f667f183f94d54f655ca5c94e570bcc3f1
Olyad Amsalu <olexyit@gmail.com>
2023-04-04 16:15:41 +0300

    some of the pages features changed

--- home.js
+++ home.js
@@ -39,8 +39,8 @@ function changeImage() {
     var second = document.getElementById('second');
     var third = document.getElementById('third');
 
-    if (image.src.match("images/Foods/beyaynet1.jpg") || second.onclick) {
-        image.src = "images/Foods/chechebsaa.jpg";
+    if (image.src.match("src/images/Foods/beyaynet1.jpg") || second.onclick) {
+        image.src = "src/images/Foods/chechebsaa.jpg";
         info.innerHTML = "Special of the day: Chechebsa<br>Payment: 100 birr<br>Address: 5 kilo";
         first.style.width = '10px'
         first.style.height = '10px'
@@ -57,7 +57,7 @@ function changeImage() {
         third.style.marginTop = '0px'
     }
     else if (image.src.match("images/Foods/chechebsaa.jpg" || third.onclick)) {
-        image.src = "images/Foods/fiirfir.jpg";
+        image.src = "src/images/Foods/fiirfir.jpg";
         info.innerHTML = "Special of the day: Firfir<br>Payment: 50 birr<br>Address: 4 kilo MotherBets"
         second.style.width = '10px'
         second.style.height = '10px'
@@ -74,7 +74,7 @@ function changeImage() {
         third.style.marginTop = '-3px'
     }
     else{
-        image.src = "images/Foods/beyaynet1.jpg"
+        image.src = "src/images/Foods/beyaynet1.jpg"
         info.innerHTML = "Special of the day: Beyaynet<br>Payment: 80 birr<br>Address: 6 kilo MotherBets"
         second.style.width = '10px'
         second.style.height = '10px'
